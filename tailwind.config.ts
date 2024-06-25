756f42b316a414ae0335732670fdbee4f42c7200
Victory Ndukwu <victoryndukwu7@gmail.comm>
2024-06-25 21:59:38 +0100

    build: add framer-motion

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        darkGray: '#45474B',
+        lightGray: '#F5F7F8'
+      }
     },
   },
   plugins: [],
