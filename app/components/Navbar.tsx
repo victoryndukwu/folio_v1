186e1ee7e872fe81cbbeed19b34548ef7205f8b8
Victory Ndukwu <victoryndukwu7@gmail.comm>
2024-07-27 17:04:35 +0100

    chore: add navigation for mobile screens

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from 'react'
-import { Sun, Moon, Menu } from 'lucide-react';
+import { Sun, Moon, Menu, X } from 'lucide-react';
 import Logo from './Logo';
 import { NavMenuItems } from '../data/data';
 import Link from 'next/link';
@@ -11,7 +11,9 @@ import { michroma, unbounded } from '@/utils/fonts';
 function Navbar() {
 
     const [darkTheme, setDarkTheme] = useState(false)
+    const [menu, showMenu] = useState(false)
     const handleDarkTheme = () => setDarkTheme(!darkTheme)
+
 
     return (
         <nav className={`grid place-items-center py-5 ${unbounded.className}`}>
@@ -28,9 +30,28 @@ function Navbar() {
                     {/* {darkTheme ?
                         (<Sun onClick={handleDarkTheme} strokeWidth={1} />) :
                         (<Moon onClick={handleDarkTheme} strokeWidth={1} />)} */}
-                    <Menu strokeWidth={1} />
+                    <Menu strokeWidth={1} onClick={() => showMenu(!menu)} />
                 </div>
             </div>
+
+            {
+                menu && (
+                    <div className='absolute bg-darkGray h-screen w-screen md:hidden top-0 left-0 flex px-4 justify-between'>
+                        <div className='flex flex-col text-white text-2xl'>
+                            {
+                                NavMenuItems.map((item, idx) => (
+                                    <Link key={idx} href={item.link} className='hover:cursor-pointer hover:font-semibold my-5'>{item.name}</Link>
+                                ))
+                            }
+                        </div>
+
+                        <div className='flex justify-between py-4'>
+                            <X color='#fff' onClick={() => showMenu(!menu)} />
+                        </div>
+                    </div>
+                )
+            }
+
         </nav>
     )
 }
