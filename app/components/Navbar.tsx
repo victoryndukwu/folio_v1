d3c0b54684bcec494e37aa97006acf03c81299a9
Victory Ndukwu <victoryndukwu7@gmail.comm>
2024-07-27 18:09:20 +0100

    chore: make the menu fullscreen

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -36,7 +36,7 @@ function Navbar() {
 
             {
                 menu && (
-                    <motion.div initial={{ y: -150, opacity: 0 }} animate={{ opacity: 1, y: 0 }} transition={{ type: 'easeIn', delay: .5 }} className='absolute bg-darkGray h-[50vh] w-screen md:hidden top-0 left-0 flex flex-col px-4'>
+                    <motion.div initial={{ y: -150, opacity: 0 }} animate={{ opacity: 1, y: 0 }} transition={{ type: 'easeIn', delay: .1 }} className='absolute bg-darkGray h-screen w-screen md:hidden top-0 left-0 flex flex-col px-4'>
                         <div className='flex justify-end py-4'>
                             <X color='#fff' onClick={() => showMenu(!menu)} />
                         </div>
