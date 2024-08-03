b9cdd7eccb8f310bdf93f103b83941e2199dd9a9
Victory Ndukwu <victoryndukwu7@gmail.comm>
2024-08-03 21:58:06 +0100

    chore: store and retrieve theme state on localstorage

--- app/components/ThemeSwitch.tsx
+++ app/components/ThemeSwitch.tsx
@@ -1,6 +1,7 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Sun, Moon } from 'lucide-react';
+import { motion } from 'framer-motion';
 
 function ThemeSwitch() {
     type Theme = 'light' | 'dark'
@@ -9,17 +10,32 @@ function ThemeSwitch() {
     function handleTheme() {
         if (theme === 'light') {
             setTheme('dark')
+            localStorage.setItem('theme', 'dark')
         } else {
             setTheme('light')
+            localStorage.setItem('theme', 'light')
         }
     }
 
+    useEffect(() => {
+        const themeStorage = localStorage.getItem('theme')
+        if (themeStorage === 'dark')
+            setTheme('dark')
+        else {
+            setTheme('light')
+        }
+    }, [])
+
     return (
-        <button className='fixed md:bottom-5 md:right-5 bottom-2 right-2 rounded-full p-4 shadow-lg animate-bounce'>
+        <motion.button initial={{opacity:0}} animate={{opacity:1}}  transition={{
+            delay: 3,
+            duration: 1,
+            type: "easeIn"
+        }} className={`fixed bottom-10 right-5 rounded-full bg-darkGray p-4 shadow-lg ${theme === 'dark' && 'bg-lightGray'}`} onClick={handleTheme}>
             {theme === 'light' ?
-                (<Sun onClick={handleTheme} strokeWidth={1} />) :
-                (<Moon onClick={handleTheme} strokeWidth={1} />)}
-        </button>
+                (<Sun strokeWidth={1} className='text-lightGray' />) :
+                (<Moon strokeWidth={1} className='text-darkGray' />)}
+        </motion.button>
     )
 }
 
